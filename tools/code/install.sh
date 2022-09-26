922790fa0cf9c66b6e1ca795555e152aaf136f3a
Rodrigo V. Del Monte <rodrigo.vieira.del.monte@zalando.de>
2022-09-26 14:14:19 +0200

    Update code settings

--- tools/code/install.sh
+++ tools/code/install.sh
@@ -17,9 +17,17 @@ codium --force --install-extension ms-python.python
 codium --force --install-extension golang.Go
 codium --force --install-extension znck.grammarly
 codium --force --install-extension stkb.rewrap
-codium --force --install-extension valentjn.vscode-ltex
+codium --force --install-extension vscode-icons-team.vscode-icons
+codium --force --install-extension hashicorp.hcl
+codium --force --install-extension streetsidesoftware.-spell-checker
+codium --force --install-extension DavidAnson.vscode-markdownlint
+codium --force --install-extension shd101wyy.markdown-preview-enhanced
+codium --force --install-extension pomdtr.excalidraw-editor
+codium --force --install-extension ms-vscode.makefile-tools
+
+# Theme
+codium --force --install-extension dracula-theme.theme-dracula
 
 # configs
 _ln "$(pwd)/tools/code/settings.json" "${HOME}/.config/VSCodium/User/settings.json"
 _ln "$(pwd)/tools/code/product.json" "${HOME}/.config/VSCodium/product.json"
-#_ln "$(pwd)/tools/code/keybindings.json" "${HOME}/.config/VSCodium/User/keybindings.json"
